653e82b69cddc5bbe4451671fdfc59d88123d277
Paolo Romagnoli <romagnoli.paolo@gmail.com>
2021-04-24 12:30:58 +0200

    Create index.php

---
+++ applications/apache/files/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Event Driven Automation Test Page</title>
+</head>
+<body>
+    <h1>Web Server Test Page</h1>
+    <?php
+    echo 'Serving this page from Host IP Address : '. $_SERVER['HTTP_HOST'];
+    ?> 
+</body>
+</html>
